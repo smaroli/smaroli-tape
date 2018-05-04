e89a78839ac6f46cedb5acb2876cda49e07a1803
smaroli <marolishravya@gmail.com>
2018-05-04 12:40:50 -0400

    First commit

--- smaroli_tape/settings.py
+++ smaroli_tape/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1(!dh-731+1xahlq(9y^^&e%dd!h#05787qwfw(mmqef5!*&*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- smaroli_tape_proj/urls.py
+++ smaroli_tape_proj/urls.py
@@ -8,5 +8,6 @@ from smaroli_tape_proj import views
 
 urlpatterns = [
     url(r'^$',views.home,name='home'),
+    url(r'^smaroli-tape',views.home,name='home'),
     url(r'^final/$',views.final,name='final')
     ]
